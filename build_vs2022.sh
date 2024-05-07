2ec035d374ab852eb3691a02fef3f7e65fde1034
arby <arbocz.marton@gmail.com>
2024-05-07 22:50:06 +0200

    minimal build

--- build_vs2022.sh
+++ build_vs2022.sh
@@ -2,6 +2,6 @@ mkdir -p out
 cd out
 
 cmake -G"Visual Studio 17 2022" ${COMMON_CMAKE_CONFIG_PARAMS} ../
-cmake --build . --config Debug
-cmake --build . --config RelWithDebInfo
-cmake --build . --config Release
+cmake --build . --config Debug --parallel
+cmake --build . --config RelWithDebInfo --parallel
+cmake --build . --config Release --parallel
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
+
+#include "dcmtk/dcmdata/dctk.h"
+#include "dcmtk/config/osconfig.h"
+
 #include <iostream>
 
 int main(int argc, char** argv)
 {
 	(void)argc;
 	(void)argv;
+
+	DcmFileFormat dcmFileFormat;
+
+	std::cout << __cplusplus;
 
 	std::cout << "test";
 
