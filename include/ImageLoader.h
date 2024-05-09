e8d45afe9ebcc985b7350dafc955f863d832bb4c
arby <arbocz.marton@gmail.com>
2024-05-09 21:39:28 +0200

    parallel image loading

--- include/ImageLoader.h
+++ include/ImageLoader.h
@@ -10,10 +10,11 @@
 class ImageLoader
 {
 public:
-    ImageLoader(const std::filesystem::path& folderPath);
+    ImageLoader(const std::filesystem::path& folderPath, const uint32_t maxThreads = 0);
 
     std::vector<std::unique_ptr<DicomImage>> load() const;
 
 private:
     const std::filesystem::path m_Folder;
+    uint32_t m_MaxThreads;
 };
--- src/ImageLoader.cpp
+++ src/ImageLoader.cpp
@@ -1,14 +1,27 @@
 #include "ImageLoader.h"
 
-ImageLoader::ImageLoader(const std::filesystem::path& folderPath)
+#include <thread>
+#include <mutex>
+
+ImageLoader::ImageLoader(const std::filesystem::path& folderPath, const uint32_t maxThreads)
     : m_Folder(folderPath)
 {
+    if (maxThreads == 0)
+    {
+        m_MaxThreads = std::thread::hardware_concurrency();
+    }
+    else
+    {
+        m_MaxThreads = maxThreads;
+    }
 }
 
 std::vector<std::unique_ptr<DicomImage>> ImageLoader::load() const
 {
     std::vector<std::unique_ptr<DicomImage>> ret;
+    std::mutex mutex;
 
+    std::vector<std::filesystem::path> dicomDirectoryEntries;
     for (const auto& dirEntry : std::filesystem::recursive_directory_iterator(m_Folder))
     {
         const auto currentPath = dirEntry.path();
@@ -17,7 +30,30 @@ std::vector<std::unique_ptr<DicomImage>> ImageLoader::load() const
             continue;
         }
 
-        ret.emplace_back(std::make_unique<DicomImage>(currentPath.string().c_str()));
+        dicomDirectoryEntries.push_back(currentPath);
+    }
+
+    const auto threadLambda = [&](const uint32_t threadID) {
+        for (uint32_t iter = threadID; iter < dicomDirectoryEntries.size(); iter += 12)
+        {
+            const auto currentPath = dicomDirectoryEntries[iter];
+            auto img = std::make_unique<DicomImage>(currentPath.string().c_str());
+
+            std::lock_guard<std::mutex> guard(mutex);
+            ret.push_back(std::move(img));
+        }
+    };
+
+    std::vector<std::thread> threads;
+    threads.reserve(m_MaxThreads);
+    for (uint32_t thread = 0; thread < m_MaxThreads; ++thread)
+    {
+        threads.emplace_back(threadLambda, thread);
+    }
+
+    for (auto& th : threads)
+    {
+        th.join();
     }
 
     return ret;
