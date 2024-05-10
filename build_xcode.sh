7645d957fea6fedf2db4af3cbf2bfca1d2074585
arboczmarton <arbocz.marton@gmail.com>
2024-05-10 18:18:10 +0200

    xcode build

---
+++ build_xcode.sh
@@ -0,0 +1,22 @@
+git submodule update --init
+
+cd thirdparty/dcmtk
+mkdir -p out
+cd out
+
+cmake -G"Xcode" ${COMMON_CMAKE_CONFIG_PARAMS} ../
+cmake --build . --config Debug --parallel
+cmake --build . --config RelWithDebInfo --parallel
+cmake --build . --config Release --parallel
+
+cd ..
+cd ..
+cd ..
+
+mkdir -p out
+cd out
+
+cmake -G"Xcode" ${COMMON_CMAKE_CONFIG_PARAMS} ../
+cmake --build . --config Debug --parallel
+cmake --build . --config RelWithDebInfo --parallel
+cmake --build . --config Release --parallel
