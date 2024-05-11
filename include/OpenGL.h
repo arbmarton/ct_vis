557d65e8a8b8cf23aa248eaf70c88c4fea9a3e57
arboczmarton <arbocz.marton@gmail.com>
2024-05-11 18:42:39 +0200

    apple specific things

--- include/OpenGL.h
+++ include/OpenGL.h
@@ -4,8 +4,13 @@
 
 struct OpenGlInfo
 {
+#ifdef __APPLE__
+    constexpr static uint8_t m_MajorVersion = 3;
+    constexpr static uint8_t m_MinorVersion = 3;
+#else
     constexpr static uint8_t m_MajorVersion = 4;
     constexpr static uint8_t m_MinorVersion = 6;
+#endif
     static std::string getVersionString()
     {
         return std::string("#version ") + std::to_string(m_MajorVersion) + std::to_string(m_MinorVersion) + std::string("0");
--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,9 @@ int main(int argc, char** argv)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OpenGlInfo::m_MajorVersion);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OpenGlInfo::m_MinorVersion);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     if constexpr (!vsync)
