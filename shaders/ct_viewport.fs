abeb25d30cd6bca3dcee52344deab36c102aee15
arby <arbocz.marton@gmail.com>
2024-05-12 16:11:28 +0200

    remove const from shader

--- shaders/ct_viewport.fs
+++ shaders/ct_viewport.fs
@@ -10,15 +10,15 @@ uniform vec3 forward;
 
 void main()
 { 
-	const vec3 right = normalize(cross(forward, vec3(0.0, 1.0, 0.0)));
-	const vec3 up = normalize(cross(right, forward));
-	const vec3 center = vec3(0.5, 0.5, 0.5);
+	vec3 right = normalize(cross(forward, vec3(0.0, 1.0, 0.0)));
+	vec3 up = normalize(cross(right, forward));
+	vec3 center = vec3(0.5, 0.5, 0.5);
 
-	const vec3 samplingPosition = center + 
+	vec3 samplingPosition = center + 
 		right * (TexCoords.x * 2.0 - 1.0) * 0.5 + 
 		up * ((1.0 - TexCoords.y) * 2.0 - 1.0) * 0.5 +
 		forward * (zLevel * 2.0 - 1.0) * 0.5;
 
-	const vec4 sampled = texture(texture3D, samplingPosition);
+	vec4 sampled = texture(texture3D, samplingPosition);
 	FragColor = vec4(sampled.r, sampled.r, sampled.r, 1.0);
 }
