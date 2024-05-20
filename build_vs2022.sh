b7cdd9d92f1ad01da5a00c72516ae283c9c2f5bf
arby <arbocz.marton@gmail.com>
2024-05-20 15:28:49 +0200

    fix windows build

--- build_vs2022.sh
+++ build_vs2022.sh
@@ -3,8 +3,8 @@
 git submodule update --init
 
 cd thirdparty/dcmtk
-mkdir -p out
-cd out
+mkdir -p build
+cd build
 
 cmake -G"Visual Studio 17 2022" -DBUILD_SHARED_LIBS=ON ${COMMON_CMAKE_CONFIG_PARAMS} ../
 cmake --build . --config Debug --parallel
